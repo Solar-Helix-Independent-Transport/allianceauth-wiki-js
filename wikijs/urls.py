514ba053ee5233524f3a39c6ba70eee78beb3ce4
Ariel Rin <ozirascal@gmail.com>
2022-07-30 22:36:21 +1000

    Django 4 compat

--- wikijs/urls.py
+++ wikijs/urls.py
@@ -1,17 +1,18 @@
-from django.conf.urls import url, include
+from django.conf.urls import include
+from django.urls import re_path
 
 from . import views
 
 app_name = 'wikijs'
 
 module_urls = [
-    url(r'^activate/$', views.activate_wikijs, name='activate'),
-    url(r'^deactivate/$', views.deactivate_wikijs, name='deactivate'),
-    url(r'^reset_password/$', views.reset_password, name='reset_password'),
-    url(r'^set_password/$', views.set_password, name='set_password'),
+    re_path(r'^activate/$', views.activate_wikijs, name='activate'),
+    re_path(r'^deactivate/$', views.deactivate_wikijs, name='deactivate'),
+    re_path(r'^reset_password/$', views.reset_password, name='reset_password'),
+    re_path(r'^set_password/$', views.set_password, name='set_password'),
 
 ]
 
 urlpatterns = [
-    url(r'^wikijs/', include((module_urls, app_name), namespace=app_name))
+    re_path(r'^wikijs/', include((module_urls, app_name), namespace=app_name))
 ]
